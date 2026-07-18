11634291f77c85bc1c4661420b45effe21f8e844
Harsh08sasuke <raih64128@gmail.com>
2026-07-18 16:31:35 +0530

    Sync LeetCode - 1934. Confirmation Rate (Medium)

---
+++ LeetCode/Medium/1934_ConfirmationRate.cpp
@@ -0,0 +1,18 @@
+/**
+ * Problem Link : https://leetcode.com/problems/confirmation-rate/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+# Write your MySQL query statement below
+SELECT s.user_id,IFNULL(ROUND(SUM(action='confirmed')/COUNT(*),2),0.00) AS confirmation_rate 
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id
+
+
+
