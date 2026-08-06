4be54149b108fb4500351dcf19def6b2e866ce45
Harsh08sasuke <raih64128@gmail.com>
2026-08-06 06:40:42 +0530

    Sync LeetCode - 205. Isomorphic Strings (Easy)

---
+++ LeetCode/Easy/205_IsomorphicStrings.java
@@ -0,0 +1,50 @@
+/**
+ * Problem Link : https://leetcode.com/problems/isomorphic-strings/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+
+
+
+
+    //public static boolean isIso(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char original = s.charAt(i);
+            char replacement = t.charAt(i);
+
+            if (!map.containsKey(original)) {
+
+                if (!map.containsValue(replacement)) {
+                    map.put(original, replacement);
+                } else {
+                    return false;
+                }
+
+            } else {
+
+                char mappedChar = map.get(original);
+
+                if (mappedChar != replacement) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+        
+    
+
